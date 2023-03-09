a6ca93868540eb12d08aef988bc5d966da5b0800
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2023-03-09 20:32:41 +0530

    successfully implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthContext from "./components/Store/Auth-Context";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const AuthCtx = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
+        {!AuthCtx.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="/profile">
+          {AuthCtx.isLoggedIn && <UserProfile />}
+          {!AuthCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AuthContext from "../Store/Auth-Context";
 
 import classes from "./MainNavigation.module.css";
@@ -7,11 +7,9 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   const AuthCtx = useContext(AuthContext);
   const isLoggedIn = AuthCtx.isLoggedIn;
-  const history = useHistory();
 
   const logoutHandler = () => {
     AuthCtx.logout();
-    history.replace("/auth");
   };
 
   return (
